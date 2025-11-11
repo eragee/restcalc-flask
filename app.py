ce888baab2d63650ac102df15ca2ce4de83469f4
Erik Agee <erik.r.agee@gmail.com>
2025-11-10 17:49:25 -0800

    Format integer answers without decimal

--- app.py
+++ app.py
@@ -11,26 +11,29 @@ def is_a_number(s):
     except ValueError:
         return False
 
+def format_number(n):
+    return int(n) if float(n).is_integer() else n
+
 @app.route('/restcalc/<arg1>/plus/<arg2>', methods=['GET'])
 def add(arg1, arg2):
     if not (is_a_number(arg1) and is_a_number(arg2)):
         return rest_error("Invalid arguments.")
     answer = float(arg1) + float(arg2)
-    return rest_response(str(answer))
+    return rest_response(str(format_number(answer)))
 
 @app.route('/restcalc/<arg1>/minus/<arg2>', methods=['GET'])
 def subtract(arg1, arg2):
     if not (is_a_number(arg1) and is_a_number(arg2)):
         return rest_error("Invalid arguments.")
     answer = float(arg1) - float(arg2)
-    return rest_response(str(answer))
+    return rest_response(str(format_number(answer)))
 
 @app.route('/restcalc/<arg1>/times/<arg2>', methods=['GET'])
 def multiply(arg1, arg2):
     if not (is_a_number(arg1) and is_a_number(arg2)):
         return rest_error("Invalid arguments.")
     answer = float(arg1) * float(arg2)
-    return rest_response(str(answer))
+    return rest_response(str(format_number(answer)))
 
 @app.route('/restcalc/<arg1>/dividedby/<arg2>', methods=['GET'])
 def divide(arg1, arg2):
@@ -40,7 +43,7 @@ def divide(arg1, arg2):
         answer = float(arg1) / float(arg2)
     except ZeroDivisionError:
         return rest_error("Division by zero.")
-    return rest_response(str(answer))
+    return rest_response(str(format_number(answer)))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
